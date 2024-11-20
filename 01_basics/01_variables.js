4dce8175e863c8eff07dc781e9d747f356572410
Sahil <ms.mdsahil77@gmail.com>
2024-11-20 18:15:15 +0000

    data-type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+let accountId = 43434
+const email = 'ms@mail.com'
+var add = 'kolkata'
+city = 'jaipur'
+
+let accountState;  // will be undefined
+
+// email = 'pp@g.com' can not assign
+
+accountId = 'op'
+add = 'mumbai'
+city ='keral'
+
+console.table([accountId, add, city, accountState]);
+
+// let and var value can be changed
+
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" ;   //used all js code as newer version 
+
+// 7 data types 
+
+// Number, String, Boolean, Symbol, BigInt, null, undefined
+
+//object
+
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
