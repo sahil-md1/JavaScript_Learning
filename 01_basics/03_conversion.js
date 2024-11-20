6b378cb6667fb5ed402a004fd28da68552b154e3
Sahil <ms.mdsahil77@gmail.com>
2024-11-20 18:40:55 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+let score = '33'
+
+let newScore= (Number(score))
+
+console.log(typeof newScore);  //--number
+
+let score2 = '33ee'
+
+let newScore2 = (Number(score2))
+
+console.log(typeof newScore2);  //--number
+
+console.log(newScore2);   //---NaN
+
+
+let loggedIn = 1 
+
+let booleanloggedIn = Boolean(loggedIn)
+
+console.log(booleanloggedIn);
+
+
+// 1 => true ; 0 => false;
+// "" => false
+// 'SAHIL' => true
+
+
+let someNum = 8989
+
+let stringNum = (String(someNum))
+
+console.log(stringNum);
+
+console.log(typeof stringNum);
+
