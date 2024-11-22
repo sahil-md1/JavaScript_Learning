6c6e73de1e6313192b3f63f644339c69eadd4516
Sahil <ms.mdsahil77@gmail.com>
2024-11-22 20:37:46 +0000

    numbers

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,41 @@
+const kite = 700
+
+console.log(kite);
+
+const kite2 = new Number(200.994)
+console.log(kite2.toFixed(2));
+console.log(kite2.toFixed(2));
+
+
+const length = 1253.22
+
+console.log(length.toPrecision(4));
+
+// 1253 -output
+
+const length2 = 12523232323233.22
+
+console.log(length2.toLocaleString());
+
+// 12,523,232,323,233.22 -Output
+
+
+// --------------------Maths--------------------------
+
+const numb = 5
+console.log(Math.round(numb));
+console.log(Math.floor(numb));
+console.log(Math.ceil(numb));
+
+
+// .absolur --- positve 
+console.log(Math.pow(numb));
+
+const min = 10
+const max = 100
+
+console.log(Math.floor((Math.random()* (max - min +1) + min  )));
+
+// oputpu will be 10-100 alwasys
+
+
