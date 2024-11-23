15bc9cb32f0e12f242a4e9bcea874b9f72304aa0
Sahil <ms.mdsahil77@gmail.com>
2024-11-23 20:27:52 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,27 @@
+function one() {
+    const username ='sahil'
+
+    function two(){
+        const newuser = 'sahil Islam'
+        console.log(username);
+        console.log(newuser);
+    }
+    two()
+    
+}
+one()
+
+
+
+function one(num){
+    return num + 2
+}
+console.log(one(5))
+
+ 
+const newFun = function(){
+    return num2 + 4
+}
+newFun(10)  
+
+// cannot access outside
