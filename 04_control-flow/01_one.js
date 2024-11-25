f3ea24dbad848b507ef923d2f75ca77f34379a63
Sahil <ms.mdsahil77@gmail.com>
2024-11-25 17:26:18 +0000

    one-if else

---
+++ 04_control-flow/01_one.js
@@ -0,0 +1,37 @@
+// < > = <= >= == != === !== 
+
+
+
+// let score =100
+// if (score < 90 ) {
+//     console.log(`correct statement `);
+// } else {
+//     console.log(`wrogn statement `);
+ 
+// }
+
+
+// let jQueryprice = 999
+
+// if (jQueryprice >1000){
+//     console.log('Wrong price');
+    
+// } else if (jQueryprice > 500){
+//     console.log('Wrong price 2');
+// }
+
+
+// let userId = true
+// let debitCard = true
+
+// if (userId && debitCard){
+// console.log('Eligible for login');
+// }
+
+let cash = true
+let creditCard = false
+
+if (cash || creditCard ){
+    console.log('Eligible for myntra shopping');
+    }
+
