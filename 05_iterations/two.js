012d67253a498236c8b2aa4b873d2800754a51cf
Sahil <ms.mdsahil77@gmail.com>
2024-12-03 18:36:44 +0000

    iterations two

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0;
+
+while (index <=10) {
+    // console.log(`The value of index will be ${index}`);
+    index= index + 2
+}
+
+
+let myArray = ['superman', 'flash', 'ironman', 'batman'];
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr +1
+}
+
+let score = 0;
+do {
+    console.log(`score will be ${score}`);
+    score++
+
+} while (score <= 10)
